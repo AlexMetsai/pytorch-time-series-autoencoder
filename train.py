466533f642ac38cd5ffb8817eab8953d6dd1c16a
Alexandros Metsai <alexmetsai@gmail.com>
2021-06-14 00:32:46 +0300

    declarations

--- train.py
+++ train.py
@@ -7,3 +7,18 @@ import torch.optim as optim
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
 from model.shallow_autoencoder import ConvAutoencoder
+
+
+# load model definition
+model = ConvAutoencoder()
+model = model.double()  # tackles a type error
+
+# define loss and optimizer
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# prepare pytorch dataloader
+X = np.random.random((300, 1, 100))  # toy data
+Y = X
+dataset = TensorDataset(torch.tensor(X), torch.tensor(Y))
+dataloader = DataLoader(dataset, batch_size=256, shuffle=True)
