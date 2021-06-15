d48fe298a61e63f31794045c9c191aea6d213aa2
Alexandros Metsai <alexmetsai@gmail.com>
2021-06-15 09:29:12 +0300

    minor

--- train.py
+++ train.py
@@ -17,9 +17,13 @@ model = model.double()  # tackles a type error
 criterion = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
+# Toy data:
+# Using separate input and output variables to cover all cases,
+# since Y could differ from X (e.g. for denoising autoencoders).
+X = np.random.random((300, 1, 100))
+Y = X  
+
 # prepare pytorch dataloader
-X = np.random.random((300, 1, 100))  # toy data
-Y = X
 dataset = TensorDataset(torch.tensor(X), torch.tensor(Y))
 dataloader = DataLoader(dataset, batch_size=256, shuffle=True)
 
