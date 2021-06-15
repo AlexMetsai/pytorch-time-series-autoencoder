c441f68358ca24a2bbdac41d5a6626091d98b2e2
Alexandros Metsai <alexmetsai@gmail.com>
2021-06-15 09:15:28 +0300

    minor PEP 8

--- models/shallow_autoencoder.py
+++ models/shallow_autoencoder.py
@@ -7,17 +7,17 @@ class ConvAutoencoder(nn.Module):
         
         super().__init__()
        
-        #Encoder
+        # Encoder
         self.conv1 = nn.Conv1d(channels, enc_dim, 7, strides, padding=0)  
         self.dropout = nn.Dropout(0.2)
         
-        #Decoder
+        # Decoder
         self.t_conv1 = nn.ConvTranspose1d(enc_dim, 1, 7, strides, padding=0)
         self.t_conv2 = nn.ConvTranspose1d(1, 1, 1, strides, padding=0)
     
     def forward(self, x):
         
-        #Encoder
+        # Encoder
         x = self.conv1(x)
         x = self.dropout(x)
         
@@ -29,7 +29,7 @@ class ConvAutoencoder(nn.Module):
 
 if __name__ == "__main__":
     
-    # Sanity check, ignore this 'main' code.
+    # Sanity check for in/out dimensions, ignore this 'main' code.
     
     model = ConvAutoencoder()
     
